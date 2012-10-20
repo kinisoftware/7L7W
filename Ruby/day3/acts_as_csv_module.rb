d034db666c98326f370fbc7fda8bfee77006b26d
kinisoftware <kinisoftware@gmail.com>
2012-10-20 18:27:10 +0200

    Added day 3

---
+++ Ruby/day3/acts_as_csv_module.rb
@@ -0,0 +1,73 @@
+#---
+# Excerpted from "Seven Languages in Seven Weeks",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/btlang for more book information.
+#---
+module ActsAsCsv
+  def self.included(base)
+    base.extend ClassMethods
+  end
+  
+  module ClassMethods
+    def acts_as_csv
+      include InstanceMethods
+    end
+  end
+  
+  module InstanceMethods   
+    def read
+      @csv_contents = []
+      filename = self.class.to_s.downcase + '.txt'
+      file = File.new(filename)
+      @headers = file.gets.chomp.split(', ')
+
+      file.each do |row|
+        @csv_contents << row.chomp.split(', ')
+      end
+    end
+    
+    attr_accessor :headers, :csv_contents
+    def initialize
+      read 
+    end
+
+    def each(&block)
+      @csv_contents.each do |row|
+        row_with_headers = {}
+        row.each_with_index do |value, index|         
+          header = @headers[index]
+          row_with_headers[header] = value
+        end
+        block.call(CsvRow.new(row_with_headers))
+      end
+    end
+  end
+end
+
+class CsvRow
+
+  def initialize(row = {})    
+    @row = row    
+  end
+
+  def method_missing name, *args
+    @row[name.to_s]
+  end
+end
+
+class RubyCsv  # no inheritance! You can mix it in
+  include ActsAsCsv
+  acts_as_csv
+end
+
+
+# m = RubyCsv.new
+# puts m.headers.inspect
+# puts m.csv_contents.inspect
+
+csv = RubyCsv.new
+csv.each {|row| puts row.one}
+csv.each {|row| puts row.two}
